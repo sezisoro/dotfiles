eaf9799514ee30cca0a3ae3c3e1ba5c2e945465b
sez <sezisoro@gmail.com>
2026-08-28 12:26:02 -0400

    Tweaking nvim lua settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,13 +27,20 @@ require('config.lazy')
 -- Basic settings
 vim.o.number = true         -- Enable line numbers
 vim.o.relativenumber = true -- Enable relative line numbers
-vim.o.tabstop = 2           -- Number of spaces a tab represents
-vim.o.shiftwidth = 2        -- Number of spaces for each indentation
-vim.o.expandtab = true      -- Convert tabs to spaces
+vim.o.tabstop = 4           -- Number of spaces a tab represents
+vim.o.shiftwidth = 4        -- Number of spaces for each indentation
+vim.o.expandtab = false     -- Convert tabs to spaces
 vim.o.smartindent = true    -- Automatically indent new lines
 vim.o.wrap = false          -- Disable line wrapping
 vim.o.cursorline = true     -- Highlight the current line
 vim.o.termguicolors = true  -- Enable 24-bit RGB colors
+-- Unicode broken in Lua. lol
+--vim.opt.listchars = { tab = '»', trail = '°', eol = '↵' }
+vim.opt.listchars = {
+	eol = '↵',
+	tab = '» ',
+	trail = '°',
+}
 
 -- Syntax highlighting and filetype plugins
 vim.cmd('syntax enable')
